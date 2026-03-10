1f08e9822ba3eead7c70257949337f9fa3bda2d3
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-03-10 10:24:10 -0700

    Updates to use the nwm-ewts libraries

--- src/bmi_topmodel.c
+++ src/bmi_topmodel.c
@@ -412,6 +412,13 @@ static int Initialize(Bmi *self, const char *cfg_file) {
     topmodel_model *topmodel;
     topmodel = (topmodel_model *)self->data;
 
+    // Initialize the Error, Warning and Trapping System
+#ifdef EWTS_HAVE_NGEN_BRIDGE    
+    EwtsInit(EWTS_ID_TOPMODEL, true);
+#else
+    EwtsInit(EWTS_ID_TOPMODEL, false);
+#endif    
+    
     // Read and setup data from file
     int ret = init_config(cfg_file, topmodel);
     if (ret != BMI_SUCCESS)
