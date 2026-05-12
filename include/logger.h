0898b98c556adb06bd92fb77677feb0589b1069f
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-05-12 08:30:50 -0700

    Remove using generic EWTS_ID.

--- include/logger.h
+++ include/logger.h
@@ -1,7 +1,13 @@
 #ifndef TOPMODEL_LOGGER_H
 #define TOPMODEL_LOGGER_H
+
 #include "ewts/module_constants.h"
-#define EWTS_ID EWTS_ID_TOPMODEL
 #include "ewts/logger.h"
 #include "ewts/log_levels.h"
+
+#define Log(level, ...) EwtsLogModule(EWTS_ID_TOPMODEL, (level), __VA_ARGS__)
+#define LOG(level, ...) EwtsLogModule(EWTS_ID_TOPMODEL, (level), __VA_ARGS__)
+#define GetLogLevel() EwtsGetLogLevelModule(EWTS_ID_TOPMODEL)
+#define IsLoggingEnabled() EwtsIsLoggingEnabledModule(EWTS_ID_TOPMODEL)
+
 #endif /* TOPMODEL_LOGGER_H */
